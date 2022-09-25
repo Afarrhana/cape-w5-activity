c0124676833b80983fa831bc93351ef5e53be4ac
Ain Farrhana Adnan <farrhana00@gmail.com>
2022-09-25 19:42:32 +0800

    update

--- src/component/challenges/Challenge3/App.js
+++ src/component/challenges/Challenge3/App.js
@@ -1,13 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-// import PageComponent1 from './component'
-// import MySection from './MySection';
-// import MyButton from './MyButton';
-// import Form from './form'
-// import Square from './square'
-// import Board from './board'
 import Game from './game'
-import PageMultimedia from '../../../cape-w5-activity/src/component/exercise6';
 
 function App() {
   return (      
